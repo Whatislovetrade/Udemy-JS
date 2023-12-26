35067337fbcc4baccb244ae41e3af9f5a585aa8b
Vlad Ivannikov <vladmaslow1@gmail.com>
2023-12-27 00:18:15 +0600

    add check count

--- index.js
+++ index.js
@@ -16,7 +16,19 @@ for (let i = 0; i < 2; i++) {
 
   if (userQuestions !== null && userQuestions2 !== null && userQuestions !== '' && userQuestions2 !== '' && userQuestions.length < 50) {
     personalMovieDB.movies[userQuestions] = userQuestions2
+  } else {
+    i--
   }
+}
+
+if (personalMovieDB.count < 10) {
+  console.log('Просмотренно довольно мало фильмов')
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+  console.log('Вы классический зритель')
+} else if (personalMovieDB.count >= 30) {
+  console.log('Вы киноман')
+} else {
+  console.log('Произошла ошибка')
 }
 
 console.log(personalMovieDB)
